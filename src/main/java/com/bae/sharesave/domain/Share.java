5e25575d2003418f39a2279d3a018b79e2df91e4
Michael-Darmody <darmodymichael@gmail.com>
2021-03-07 14:18:23 +0000

    Created repository and updated share class to entity

--- src/main/java/com/bae/sharesave/domain/Share.java
+++ src/main/java/com/bae/sharesave/domain/Share.java
@@ -1,11 +1,29 @@
 package com.bae.sharesave.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Share {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	private String name;
 	private double amount;
 	private double price;
 	private double value;
+
+	public Share(Long id, String name, double amount, double price, double value) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.amount = amount;
+		this.price = price;
+		this.value = value;
+	}
 
 	public Share(String name, double amount, double price, double value) {
 		super();
@@ -49,6 +67,14 @@ public class Share {
 
 	public void setValue(double value) {
 		this.value = value;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 }

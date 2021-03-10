ede6d486369e81d7ba0e89727c003da0d8423a0c
Michael-Darmody <darmodymichael@gmail.com>
2021-03-10 16:53:35 +0000

    Added Put mapping to share controller

--- src/main/java/com/bae/sharesave/rest/ShareController.java
+++ src/main/java/com/bae/sharesave/rest/ShareController.java
@@ -3,7 +3,9 @@ package com.bae.sharesave.rest;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,10 @@ public class ShareController {
 	@GetMapping("/getShares")
 	public List<Share> getShares() {
 		return this.service.getShares();
+	}
+
+	@PutMapping("/update/{id}")
+	public Share updatePerson(@PathVariable Long id, @RequestBody Share newShare) {
+		return this.service.updateShare(id, newShare);
 	}
 }

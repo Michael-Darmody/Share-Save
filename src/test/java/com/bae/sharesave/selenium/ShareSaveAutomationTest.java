c74a7580c66081873fa1c1daae213acc45a8d483
Michael-Darmody <darmodymichael@gmail.com>
2021-03-13 19:51:24 +0000

    Correctly implemented the h2 database for the automation tests to run

--- src/test/java/com/bae/sharesave/selenium/ShareSaveAutomationTest.java
+++ src/test/java/com/bae/sharesave/selenium/ShareSaveAutomationTest.java
@@ -1,6 +1,5 @@
 package com.bae.sharesave.selenium;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,9 +10,13 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@Sql(scripts = { "classpath:share-schema.sql",
+		"classpath:share-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 public class ShareSaveAutomationTest {
 
 	private RemoteWebDriver driver;
@@ -25,17 +28,18 @@ public class ShareSaveAutomationTest {
 	}
 
 	@Test
-	void testShareSaveRead() {
+	void testShareSaveRead() throws InterruptedException {
 		this.driver.get("http://127.0.0.1:5500/ShareSave/index.html");
 
-		WebElement cardTitle = this.driver
-				.findElement(By.xpath("//*[@id=\"output\"]/section[1]/section/section[1]/h5"));
+		Thread.sleep(2000);
+
+		WebElement cardTitle = this.driver.findElement(By.xpath("//*[@id=\"output\"]/section/section/section[1]/h5"));
 
 		Assertions.assertTrue(cardTitle.getText().contains("Barclays"));
 	}
-
-	@AfterEach
-	void tearDown() {
-		this.driver.close();
-	}
+//
+//	@AfterEach
+//	void tearDown() {
+//		this.driver.close();
+//	}
 }
